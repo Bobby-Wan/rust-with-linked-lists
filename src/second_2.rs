557853699eaa0d38fb53361430b5622fde146931
Bobby-Wan <bobbie.at.97@gmail.com>
2024-02-19 13:46:11 +0200

    Second list second read-through.

---
+++ src/second_2.rs
@@ -0,0 +1,158 @@
+use std::mem;
+
+pub struct List<T> {
+    head: Link<T>
+}
+pub struct IntoIter<T>(List<T>);
+pub struct Iter<'a, T>(Option<&'a Node<T>>);
+pub struct IterMut<'a, T>(Option<&'a mut Node<T>>);
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+impl <T> List<T> {
+    pub fn new() -> Self {
+        List { head: Link::None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take()
+        });
+        self.head = Link::Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+
+        }) 
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        //TODO: spend more time on as_ref
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(& self) -> Iter<'_, T> {
+        // Figure out the difference between the two, esp the as_deref call
+        // Iter(self.head.as_ref().map(|node_box| node_box.as_ref()))
+        Iter(self.head.as_deref())
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut(self.head.as_deref_mut())
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item>  {
+        self.0.map(|node| {
+            self.0 = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item>  {
+        self.0.take().map(|node| {
+            self.0 = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+impl <T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+
+        while let Link::Some(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3); 
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+    }
+}
